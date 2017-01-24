8554cf05262ab6ad6e8da5f11022e5dc2a452e38
Peter Hutterer <peter.hutterer@who-t.net>
2017-01-24 10:32:07 +1000

    autogen.sh: use exec instead of waiting for configure to finish
    Syncs the invocation of configure with the one from the server.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

--- autogen.sh
+++ autogen.sh
@@ -13,5 +13,5 @@ git config --local --get format.subjectPrefix ||
         git config --local format.subjectPrefix "PATCH libxtrans"
 
 if test -z "$NOCONFIGURE"; then
-    $srcdir/configure "$@"
+    exec $srcdir/configure "$@"
 fi
